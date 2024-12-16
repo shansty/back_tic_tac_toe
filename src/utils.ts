f4c2708f1ce24edb3f46d0601d0d07b6adfafe5a
Anastasiya <shirochina16@gmail.com>
2024-12-16 11:46:50 +0100

    Add hash functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { randomBytes, scrypt as scryptCallback } from "crypto";
+import { promisify } from "util";
+
+
 export function generateRandomString(length: number): string {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
@@ -6,3 +10,20 @@ export function generateRandomString(length: number): string {
     }
     return result;
 }
+
+
+const scrypt: (password: string | Buffer, salt: string | Buffer, keylen: number) => Promise<Buffer> = promisify(scryptCallback);
+
+export const scryptHash = async (string: string, salt?: string): Promise<string> => {
+    const saltInUse = salt || randomBytes(16).toString("hex");
+    const hashBuffer = await scrypt(string, saltInUse, 32);
+
+    return `${hashBuffer.toString("hex")}:${saltInUse}`;
+};
+
+export const scryptVerify = async (string: string, hashAndSalt: string): Promise<boolean> => {
+    const [hash, salt] = hashAndSalt.split(":");
+    const newHashAndSalt = await scryptHash(string, salt);
+
+    return newHashAndSalt === hashAndSalt;
+};
